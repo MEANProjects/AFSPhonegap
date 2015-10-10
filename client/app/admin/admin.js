6f2b306aaaaabb920b8c46398462b1fd64f99f12
chandrasekar kannaiah <chanderk@gmail.com>
2015-10-10 13:21:59 +0300

    initial commit

---
+++ client/app/admin/admin.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('afsphonegapApp')
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/admin', {
+        templateUrl: 'app/admin/admin.html',
+        controller: 'AdminCtrl'
+      });
+  });
