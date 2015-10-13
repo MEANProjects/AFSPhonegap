68d56e24774fe5208c25b0a6888cc977a0e4f9af
chandrasekar kannaiah <chanderk@gmail.com>
2015-10-13 23:20:26 +0300

    adding phonegap app
    Signed-off-by: chandrasekar kannaiah <chanderk@gmail.com>

--- client/components/services/config.service.js
+++ client/components/services/config.service.js
@@ -1,3 +1,3 @@
 angular.module('afsphonegapApp').service('constants', function () {
-  this.BACKEND_URL = 'http://localhost:9000';
+  this.BACKEND_URL = 'http://192.168.0.110:9000';  //ur ip to test the app in LAN
 });
